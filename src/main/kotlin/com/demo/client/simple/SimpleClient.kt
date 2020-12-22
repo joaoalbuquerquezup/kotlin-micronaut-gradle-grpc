aa9e12951ac7f8a18a2cb809d45ac8f2b035ccbe
Joao Albuquerque <joao.albuquerque@zup.com.br>
2020-12-22 00:50:54 -0300

    refactor (main): using GrpcKt over GrpcJava

--- src/main/kotlin/com/demo/client/simple/SimpleClient.kt
+++ src/main/kotlin/com/demo/client/simple/SimpleClient.kt
@@ -2,9 +2,21 @@ package com.demo.client.simple
 
 import com.demo.simple.SimpleRequest
 import com.demo.simple.SimpleServiceGrpc
+import com.demo.simple.SimpleServiceGrpcKt
 import io.grpc.ManagedChannelBuilder
 
-fun main() {
+suspend fun main() {
+    val channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+        .usePlaintext()
+        .build()
+    val stub = SimpleServiceGrpcKt.SimpleServiceCoroutineStub(channel)
+    val helloResponse = stub.send(SimpleRequest.newBuilder().setName("testando o client").build())
+    val message = helloResponse.message
+    println(message)
+    channel.shutdown()
+}
+
+fun mainJavaStub() {
     val channel = ManagedChannelBuilder.forAddress("localhost", 50051)
                                         .usePlaintext()
                                         .build()
